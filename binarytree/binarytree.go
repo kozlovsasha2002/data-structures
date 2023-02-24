4668d6399323be0dedd8f9468525459727797cc4
kozlovsasha2002 <kozlov_aleksanr24@mail.ru>
2023-02-24 17:32:29 +0300

    Implemented all methods of the binarytree package.

---
+++ binarytree/binarytree.go
@@ -0,0 +1,130 @@
+package binarytree
+
+import (
+	"fmt"
+	"math"
+)
+
+type Treenode struct {
+	value int
+	left  *Treenode
+	right *Treenode
+}
+
+func Add(tree *Treenode, value int) *Treenode {
+	if tree == nil {
+		tree = create(value)
+	} else if value < tree.value {
+		tree.left = Add(tree.left, value)
+	} else {
+		tree.right = Add(tree.right, value)
+	}
+	return tree
+}
+
+func create(value int) *Treenode {
+	return &Treenode{value: value, left: nil, right: nil}
+}
+
+func Print(tree *Treenode) {
+	if tree != nil {
+		fmt.Print(tree.value, " ")
+		Print(tree.left)
+		Print(tree.right)
+	}
+}
+
+func DepthFirstSearch(tree *Treenode) {
+	if tree != nil {
+		DepthFirstSearch(tree.left)
+		fmt.Print(tree.value, " ")
+		DepthFirstSearch(tree.right)
+	}
+}
+
+func Find(tree *Treenode, value int) *int {
+	if &tree.value == nil {
+		return nil
+	}
+	if tree.value == value {
+		return &tree.value
+	}
+	if tree.value < value {
+		return Find(tree.right, value)
+	}
+	if tree.value > value {
+		return Find(tree.left, value)
+	}
+	return nil
+}
+
+func Delete(tree *Treenode, value int) *Treenode {
+	if tree == nil {
+		return tree
+	}
+
+	if value < tree.value {
+		tree.left = Delete(tree.left, value)
+	} else if value > tree.value {
+		tree.right = Delete(tree.right, value)
+	} else {
+		if tree.left == nil {
+			temp := tree.right
+			tree = nil
+			return temp
+		} else if tree.right == nil {
+			temp := tree.left
+			tree = nil
+			return temp
+		}
+		temp := minValue(tree.right)
+
+		tree.value = temp.value
+		tree.right = Delete(tree.right, temp.value)
+	}
+	return tree
+}
+
+func minValue(tree *Treenode) *Treenode {
+	current := tree
+	for current.left != nil {
+		current = current.left
+	}
+	return current
+}
+
+func IsEmpty(tree *Treenode) bool {
+	if tree == nil {
+		return true
+	}
+	return false
+}
+
+func BreadthFirstSearch(tree *Treenode) {
+	h := height(tree)
+	for i := 0; i < h; i++ {
+		printLevel(tree, i)
+	}
+}
+
+func printLevel(tree *Treenode, level int) {
+	if tree == nil {
+		return
+	}
+	if level == 0 {
+		fmt.Print(tree.value, " ")
+	} else if level > 0 {
+		printLevel(tree.left, level-1)
+		printLevel(tree.right, level-1)
+	}
+}
+
+func height(tree *Treenode) int {
+	if tree == nil {
+		return 0
+	}
+	lHeight := height(tree.left)
+	rHeight := height(tree.right)
+
+	return int(math.Max(float64(lHeight), float64(rHeight))) + 1
+}
