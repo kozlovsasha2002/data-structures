1c217a6c9ad699b8ff13b99c0bdd8b5a0a74b322
kozlovsasha2002 <kozlov_aleksanr24@mail.ru>
2023-03-14 18:51:01 +0300

    Implemented all the functionality of the prefixtree package. Added tests for exported methods.

--- prefixtree/prefix_tree.go
+++ prefixtree/prefix_tree.go
@@ -1,1 +1,138 @@
 package prefixtree
+
+import (
+	"errors"
+	"fmt"
+)
+
+type PrefixTree struct {
+	head                  *vertex
+	amountOfAddedElements int
+}
+
+func New() *PrefixTree {
+	return &PrefixTree{head: newVertex(), amountOfAddedElements: 0}
+}
+
+type vertex struct {
+	nodes      map[string]*vertex
+	data       string
+	isTerminal bool
+}
+
+func newVertex() *vertex {
+	return &vertex{nodes: make(map[string]*vertex), data: "", isTerminal: false}
+}
+
+func (p *PrefixTree) Add(str string) bool {
+	if str == "" {
+		return false
+	}
+
+	current := p.head
+	for _, ch := range str {
+		char := string(ch)
+
+		if current.nodes[char] == nil {
+			current.nodes[char] = &vertex{nodes: make(map[string]*vertex), data: "", isTerminal: false}
+		}
+		current = current.nodes[char]
+	}
+	current.isTerminal = true
+	current.data = str
+	p.amountOfAddedElements++
+	return true
+}
+
+func (p *PrefixTree) AddAll(strings []string) error {
+	if len(strings) == 0 {
+		return errors.New("can't add an empty slice")
+	}
+
+	for _, str := range strings {
+		isSuccess := p.Add(str)
+		if !isSuccess {
+			return errors.New("can't insert empty string")
+		}
+	}
+	return nil
+}
+
+func (p *PrefixTree) Contains(str string) bool {
+	current := p.head
+	for _, ch := range str {
+		char := string(ch)
+
+		if current.nodes[char] == nil {
+			return false
+		}
+		current = current.nodes[char]
+	}
+	if current.isTerminal == false {
+		return false
+	}
+	return true
+}
+
+func (p *PrefixTree) Remove(str string) bool {
+	current := p.head
+	for _, ch := range str {
+		char := string(ch)
+
+		if current.nodes[char] == nil {
+			return false
+		}
+		current = current.nodes[char]
+	}
+
+	if current.isTerminal == false {
+		return false
+	} else {
+		current.isTerminal = false
+		current.data = ""
+		p.amountOfAddedElements--
+		return true
+	}
+}
+
+func (p *PrefixTree) GetAllByPrefix(prefix string) ([]string, error) {
+	result := make([]string, 0)
+
+	if len(p.head.nodes) == 0 {
+		return nil, errors.New("tree is empty")
+	}
+
+	current := p.head
+	for _, ch := range prefix {
+		char := string(ch)
+
+		if current.nodes[char] == nil {
+			err := fmt.Sprintf("no words found beginning with prefix = \"%s\"", prefix)
+			return nil, errors.New(err)
+		}
+		current = current.nodes[char]
+	}
+
+	if current.isTerminal == true {
+		return append(result, prefix), nil
+	} else {
+		result = addTerminalStrings(result, current)
+	}
+	return result, nil
+}
+
+func (p *PrefixTree) AmountOfAddedElements() int {
+	return p.amountOfAddedElements
+}
+
+func addTerminalStrings(result []string, v *vertex) []string {
+	for _, str := range v.nodes {
+		if str != nil && str.isTerminal == true {
+			result = append(result, str.data)
+		}
+		if str != nil && str.isTerminal == false || len(str.nodes) != 0 {
+			result = addTerminalStrings(result, str)
+		}
+	}
+	return result
+}
--- prefixtree/prefixe_tree_test.go
+++ prefixtree/prefixe_tree_test.go
@@ -1,1 +1,364 @@
 package prefixtree
+
+import (
+	"testing"
+)
+
+func TestPrefixTree_AmountOfAddedElements(t *testing.T) {
+	t.Run("tree is empty", func(t *testing.T) {
+		root := New()
+
+		actual := root.AmountOfAddedElements()
+		expected := 0
+
+		if actual != expected {
+			t.Errorf("expected amount of added elements = %d. Real amount = %d", expected, actual)
+		}
+	})
+
+	t.Run("tree contains multiple elements", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "april", "arc", "boy", "book", "boss", "barrel", "bar", "base")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		actual := root.AmountOfAddedElements()
+		expected := 8
+
+		if actual != expected {
+			t.Errorf("expected amount of added elements = %d. Real amount = %d", expected, actual)
+		}
+	})
+
+	t.Run("after performing add operation", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "april", "arc", "boy", "book", "boss", "barrel", "bar", "base")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		root.Add("case")
+		actual := root.AmountOfAddedElements()
+		expected := 9
+
+		if actual != expected {
+			t.Errorf("expected amount of added elements = %d. Real amount = %d", expected, actual)
+		}
+	})
+
+	t.Run("after performing remove operation", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "april", "arc", "boy", "book", "boss", "barrel", "bar", "base")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		root.Remove("bar")
+		actual := root.AmountOfAddedElements()
+		expected := 7
+
+		if actual != expected {
+			t.Errorf("expected amount of added elements = %d. Real amount = %d", expected, actual)
+		}
+	})
+}
+
+func TestPrefixTree_Add(t *testing.T) {
+	t.Run("tree is empty. Adding a non-null string", func(t *testing.T) {
+		root := New()
+
+		root.Add("rest")
+		actualResult := root.Contains("rest")
+		expectedResult := true
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result of adding element: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("tree is not empty", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char", "chain")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		root.Add("test")
+		actualResult := root.Contains("test")
+		expectedResult := true
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result of adding element: %t and real result: %t", expectedResult, actualResult)
+		}
+
+	})
+}
+
+func TestPrefixTree_AddAll(t *testing.T) {
+	t.Run("adding zero elements", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+
+		actualError := root.AddAll(inputData)
+		expectedError := "can't add an empty slice"
+
+		if actualError.Error() != expectedError {
+			t.Errorf("expected result: %s and real result: %s", expectedError, actualError.Error())
+		}
+	})
+
+	t.Run("adding one element", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "apple")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		actualResult := root.Contains("apple")
+		expectedResult := true
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("adding multiple elements", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		actualResult := true
+		expectedResult := true
+
+		for _, item := range inputData {
+			if !root.Contains(item) {
+				actualResult = false
+			}
+		}
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("adding multiple elements with empty string", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char", "")
+
+		actualError := root.AddAll(inputData)
+		expectedError := "can't insert empty string"
+
+		if actualError.Error() != expectedError {
+			t.Errorf("expected result: %s and real result: %s", expectedError, actualError.Error())
+		}
+	})
+}
+
+func TestPrefixTree_Contains(t *testing.T) {
+	t.Run("tree is empty. Desired element = \"\"", func(t *testing.T) {
+		root := New()
+
+		actualResult := root.Contains("")
+		expectedResult := false
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("tree is empty", func(t *testing.T) {
+		root := New()
+
+		actualResult := root.Contains("any")
+		expectedResult := false
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("tree is not empty. Tree contains the desired element", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char", "chain", "apple", "april")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		actualResult := root.Contains("april")
+		expectedResult := true
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("tree is not empty. Tree doesn't contain the desired element", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char", "chain", "apple", "april")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		actualResult := root.Contains("china")
+		expectedResult := false
+
+		if actualResult != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, actualResult)
+		}
+	})
+}
+
+func TestPrefixTree_Remove(t *testing.T) {
+	t.Run("tree is empty. Removing empty string", func(t *testing.T) {
+		root := New()
+
+		result := root.Remove("")
+		expectedResult := false
+
+		if result != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, result)
+		}
+	})
+
+	t.Run("tree is empty. Removing non-empty string", func(t *testing.T) {
+		root := New()
+
+		result := root.Remove("any")
+		expectedResult := false
+
+		if result != expectedResult {
+			t.Errorf("expected result: %t and real result: %t", expectedResult, result)
+		}
+	})
+
+	t.Run("tree is not empty. Removing existing element", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char", "chain")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		result := root.Remove("cherry")
+		expectedResult := true
+
+		isDeleted := root.Contains("cherry")
+
+		if result != expectedResult {
+			t.Errorf("expected result:%t, real result:%t", expectedResult, result)
+		}
+		if isDeleted {
+			t.Errorf("element was not removed")
+		}
+	})
+
+	t.Run("tree is empty. Removing non-existing element", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "chef", "cherry", "char", "chain")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		result := root.Remove("cheese")
+		expectedResult := false
+
+		if result != expectedResult {
+			t.Errorf("expected result:%t, real result:%t", expectedResult, result)
+		}
+	})
+}
+
+func TestPrefixTree_GetAllByPrefix(t *testing.T) {
+	t.Run("tree is empty", func(t *testing.T) {
+		root := New()
+
+		_, err := root.GetAllByPrefix("be")
+		expectedError := "tree is empty"
+
+		if err.Error() != expectedError {
+			t.Errorf("expected error:%s and real error:%s", expectedError, err.Error())
+		}
+	})
+
+	t.Run("tree contains strings with entered prefix", func(t *testing.T) {
+		root := New()
+		inputData := make([]string, 0)
+		inputData = append(inputData, "april", "arc", "boy", "book", "boss", "barrel", "bar", "base")
+		err := root.AddAll(inputData)
+		if err != nil {
+			panic(err.Error())
+		}
+		expectedData := make([]string, 0)
+		expectedData = append(expectedData, "bar", "barrel", "base")
+
+		result, _ := root.GetAllByPrefix("ba")
+		actualResult := equals(result, expectedData, t)
+		expectedResult := true
+
+		if expectedResult != actualResult {
+			t.Errorf("expected result:%t and real result:%t", expectedResult, actualResult)
+		}
+	})
+
+	t.Run("tree doesn't contain strings with entered prefix", func(t *testing.T) {
+		root := New()
+		testData := make([]string, 0)
+		testData = append(testData, "april", "arc", "boy", "book", "boss", "barrel", "bar", "base")
+		err := root.AddAll(testData)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		_, err = root.GetAllByPrefix("be")
+		expectedError := "no words found beginning with prefix = \"be\""
+
+		if err.Error() != expectedError {
+			t.Errorf("expected error:%s and real error:%s", expectedError, err.Error())
+		}
+	})
+}
+
+func equals(input []string, expectedData []string, t *testing.T) bool {
+	t.Helper()
+	if len(input) != len(expectedData) {
+		return false
+	}
+
+	contains := 0
+	for _, item := range expectedData {
+		for _, item2 := range input {
+			if item == item2 {
+				contains++
+			}
+		}
+	}
+
+	if contains != len(expectedData) {
+		return false
+	} else {
+		return true
+	}
+}
